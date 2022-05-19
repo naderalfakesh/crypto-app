e6cb35b141968c50746eba3ef96714f26463a2ca
Nader Alfakesh <nader.alfakesh@gmail.com>
2022-05-19 14:34:32 +0300

    Update home screen

--- app/screens/home/home-screen.tsx
+++ app/screens/home/home-screen.tsx
@@ -1,58 +1,74 @@
 import React, { FC } from "react"
-import { ImageStyle, TextStyle, View, ViewStyle } from "react-native"
+import { View, StyleSheet } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../../navigators"
-import { AutoImage, Avatar, Screen, Text } from "../../components"
-import { color, spacing, typography } from "../../theme"
+import { AutoImage, Button, MarketStatsList, Screen, TrendingCoins } from "../../components"
 import Balance from "./Balance"
 import Header from "./Header"
+import { Coin } from "../../models/coin"
 
 const bg = require("../../../assets/images/bg.png")
 
-const ROOT: ViewStyle = {
-  backgroundColor: "transparent",
-  flex: 1,
-}
+const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
 
-const TEXT: TextStyle = {
-  color: color.palette.white,
-  fontFamily: typography.primary,
-}
-
-const BOLD: TextStyle = { fontWeight: "bold" }
-
-const HEADER: TextStyle = {
-  paddingTop: spacing[3],
-  paddingBottom: spacing[4] + spacing[1],
-  paddingHorizontal: 0,
-}
-
-const HEADER_TITLE: TextStyle = {
-  ...TEXT,
-  ...BOLD,
-  fontSize: 12,
-  lineHeight: 15,
-  textAlign: "center",
-  letterSpacing: 1.5,
-}
-
-const backgroundStyle: ImageStyle = {
-  position: "absolute",
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  resizeMode: "stretch",
-}
+const coinList: Coin[] = [
+  {
+    id: "bitcoin",
+    title: "Bitcoin",
+    subtitle: "Btc",
+    theme: "purple",
+    change: "+10.32%",
+    price: "$6780",
+    data,
+  },
+  {
+    id: "ethereum",
+    title: "Ethereum",
+    subtitle: "Eth",
+    theme: "blue",
+    change: "+10.32%",
+    price: "$6780",
+    data,
+  },
+]
 
 export const HomeScreen: FC<StackScreenProps<NavigatorParamList, "home">> = function HomeScreen() {
   return (
-    <View style={ROOT}>
-      <AutoImage source={bg} style={backgroundStyle} />
-      <Screen style={ROOT} backgroundColor="transparent" preset="fixed">
+    <View style={styles.root}>
+      <AutoImage source={bg} style={styles.background} />
+      <Screen style={styles.root} backgroundColor="transparent" preset="scroll">
         <Header />
         <Balance />
+        <TrendingCoins coinList={[...coinList, ...coinList, ...coinList]} />
+        <MarketStatsList coinList={[...coinList, ...coinList]} />
+        <View style={styles.buttonsContainer}>
+          <Button style={styles.button} preset="success" text="Deposit" />
+          <Button style={styles.button} preset="danger" text="Withdraw" />
+        </View>
       </Screen>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: "transparent",
+    flex: 1,
+  },
+  background: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    resizeMode: "stretch",
+  },
+  button: { flex: 1, marginHorizontal: 10 },
+  buttonsContainer: {
+    marginTop: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+})
